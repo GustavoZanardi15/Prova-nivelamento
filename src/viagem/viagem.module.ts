41ce5adc0cae1391f8eb3f5998d81c2ca25a16d1
GustavoZanardi15 <ulianzanardig@gmail.com>
2025-09-16 21:09:37 -0300

    feat: Adiciona name e Schema de destino

--- src/viagem/viagem.module.ts
+++ src/viagem/viagem.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ViagemService } from './viagem.service';
 import { ViagemController } from './viagem.controller';
 import { Viagem, ViagemSchema } from './schemas/viagem.schemas';
+import { Destino, DestinoSchema } from './schemas/destino.schemas';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Viagem.name, schema: ViagemSchema }]),
+    MongooseModule.forFeature([
+      { name: Viagem.name, schema: ViagemSchema },
+      { name: Destino.name, schema: DestinoSchema },
+    ]),
   ],
   controllers: [ViagemController],
   providers: [ViagemService],
